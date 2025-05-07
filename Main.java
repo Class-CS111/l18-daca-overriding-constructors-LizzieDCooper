207825e02f0d32db5c5a4fa611a0106985d1e8fe
Lizzie <cooperedanielle@gmail.com>
2025-05-07 07:04:01 +0000

    commit

--- Main.java
+++ Main.java
@@ -8,6 +8,13 @@ class Main
 {
   public static void main(String[] args) 
 	{
+      DACArecipient test = new DACArecipient();
+
+      System.out.println("Test: " + test);
+
+      DACArecipient test2 = new DACArecipient("Cooper", "Lizzie", "12-06-006", "US", 2415021,2415021,2415021,'F');
+
+      System.out.println("Test: " + test2);
 
   }
 }
